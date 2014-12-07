73d42eedaccecd00bda7f86dcb7853e5b061b7b5
Steven Dee <steve@smartercode.net>
2014-12-07 02:08:38 -0500

    Build with -std=c99

--- src/registry.c
+++ src/registry.c
@@ -15,6 +15,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#define _POSIX_C_SOURCE 200809L
 #include <search.h>
 #include <stdlib.h>
 #include "hammer.h"
