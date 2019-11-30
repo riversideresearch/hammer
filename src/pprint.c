4d287cc43a68e43289cf06b8d8de7600945f137e
Sven M. Hallberg <pesco@khjk.org>
2019-11-30 18:31:02 +0100

    oops, forgot to escape " and \

--- src/pprint.c
+++ src/pprint.c
@@ -35,7 +35,9 @@ static void pprint_bytes(FILE *stream, const uint8_t *bs, size_t len)
     fprintf(stream, "\"");
     for (size_t i = 0; i < len; i++) {
       uint8_t c = bs[i];
-      if (c >= 0x20 && c <= 0x7e)
+      if (c == '"' || c == '\\')
+        fprintf(stream, "\\%c", c);
+      else if (c >= 0x20 && c <= 0x7e)
         fputc(c, stream);
       else
         fprintf(stream, "\\u00%02hhx", c);
